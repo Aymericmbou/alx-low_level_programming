8ffd056c1765f106897056101fd8a8b17199ce16
Aymericmbou <aymericmbou@gmail.com>
2022-03-12 12:47:12 -0800

    first C programm test3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 
-int main (void)
+int main(void)
 {	
-	puts ("programming is like building a multilingual puzzle\n");
+	puts("programming is like building a multilingual puzzle\n");
 
 	return (0); 
 }
