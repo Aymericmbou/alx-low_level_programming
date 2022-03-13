feb11265f6500947cf3b4e797535ea9da7820958
Aymericmbou <aymericmbou@gmail.com>
2022-03-13 04:49:05 -0700

    firt C programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints phrase programming is like building a multilingual puzzle
- * return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
