82910bd3f4f55fc6678f03a7f40bd78d07f5b9cd
Aymericmbou <aymericmbou@gmail.com>
2022-03-13 04:17:28 -0700

    firt C programm

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main prints:"programming is like building a multilingual puzzle"
+ * main - prints:"programming is like building a multilingual puzzle"
  * return: 0 if exited properly
  */
-int main(void)
+int main (void)
 {
 	puts ("\"programming is like building a multilingual puzzle");
 	return (0);
