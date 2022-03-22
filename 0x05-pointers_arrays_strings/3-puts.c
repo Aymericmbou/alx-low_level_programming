b6239f490c547ffc7514d70891642b66880c6ddd
Aymericmbou <aymericmbou@gmail.com>
2022-03-22 12:34:35 -0700

    function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "math.h"
+
+
+/**
+ * _puts - Write a function that prints a string
+ * @str: value
+ * Return: None
+ */
+
+void _puts(char *str)
+{
+	int k;
+
+	for (k = 0; str[k] != '\0'; k++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
