eecdc4a1fb7a58998601b2361d59fb13b2570da3
Aymericmbou <aymericmbou@gmail.com>
2022-03-24 11:58:30 -0700

    concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate strings
+ * @dest: value
+ * @src: value
+ * Return: pointer to the resulting string dest 
+ */
+char _strcat(char *dest, char *src)
+{
+	char *strcat;
+
+	*strcat = *dest + *src;
+
+	return (*strcat);
+}
